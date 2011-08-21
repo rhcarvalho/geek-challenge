385e4fa74ac11fe7cf95430e56118e5b5478cf01
Rodolfo Carvalho <rhcarvalho@gmail.com>
2011-08-21 01:20:36 -0300

    Refactor to eliminate duplication and reorganize code

--- challenge-2.rkt
+++ challenge-2.rkt
@@ -1,6 +1,8 @@
 #lang racket/base
 (require racket/function
+         racket/list
          racket/port
+         racket/string
          tests/eli-tester)
 
 ;; Second stage: solve challenge for 11 and 13 digits
@@ -14,58 +16,56 @@ I can do that, since I have 7-digit primes already.
 
 (define (main)
   (printf "First palindromic prime of 11 digits found in π:\n")
-  (printf "~a\n" (first-palindromic-prime/11))
+  (printf "~a\n" (first-palindromic-prime 11))
   (printf "First palindromic prime of 13 digits found in π:\n")
-  (printf "~a\n" (first-palindromic-prime/13)))
+  (printf "~a\n" (first-palindromic-prime 13)))
 
-; -> byte-string
-(define (first-palindromic-prime/11)
+; number -> number
+(define (first-palindromic-prime digits)
   (call-with-input-file "pi100M.txt" ; pi as a string of 100,000,000 digits
     (λ (pi)
-      (for/first ([palindrome/11 (in-producer next-odd-palindrome/11 #f pi)]
-                  #:when (is-prime? palindrome/11))
-        palindrome/11))))
+      (for/first ([palindrome (in-producer (next-odd-palindrome/gen digits) #f pi)]
+                  #:when (is-prime? palindrome))
+        palindrome))))
 
-; port -> (or number #f)
-(define (next-odd-palindrome/11 port)
-  ; rewind port if necessary, so that
-  ; overlapping palidromes are considered
-  (let ([pos (file-position port)])
-    (unless (= 0 pos)
-      (file-position port (- pos 6))))
-  (let ([match->number (compose1 string->number bytes->string/utf-8 car)])
-    (cond [(regexp-match #px"([13579])(\\d)(\\d)(\\d)(\\d)(\\d)\\5\\4\\3\\2\\1" port) => match->number]
-          [else #f])))
+; number -> procedure
+(define (next-odd-palindrome/gen digits)
+  ; port -> (or number #f)
+  (λ (port)
+    ; rewind port if necessary, so that
+    ; overlapping palidromes are considered
+    (let ([pos (file-position port)])
+      (unless (= 0 pos)
+        (file-position port (- pos (/ (add1 digits) 2)))))
+    (let ([odd-palindrome (pregexp (odd-palindrome-pattern digits))]
+          [match->number (compose1 string->number bytes->string/utf-8 car)])
+      (cond [(regexp-match odd-palindrome port) => match->number]
+            [else #f]))))
 
-(test
- (call-with-input-string
-  "123457543212345734450605443"
-  (λ (s)
-    (for/list ([i (in-producer next-odd-palindrome/11 #f s)])
-      i)))
- =>
- '(12345754321
-   75432123457
-   34450605443))
+; build a pattern to find odd palindromes.
+; number -> string
+(define (odd-palindrome-pattern digits)
+  (let* ([k (/ (sub1 digits) 2)]
+         [k- (floor k)]
+         [k+ (ceiling k)])
+    (string-append* "([13579])"
+                    (string-repeat k- "(\\d)")
+                    (for/list ([i (in-range k+ 0 -1)])
+                      (format "\\~a" i)))))
 
-; -> byte-string
-(define (first-palindromic-prime/13)
-  (call-with-input-file "pi100M.txt" ; pi as a string of 100,000,000 digits
-    (λ (pi)
-      (for/first ([palindrome/13 (in-producer next-odd-palindrome/13 #f pi)]
-                  #:when (is-prime? palindrome/13))
-        palindrome/13))))
+; number -> boolean
+(define (is-prime? n)
+  (not
+   (for/or ([prime (stop-before (in-list primes/1M) (curry <= (sqrt n)))])
+     (divisible? n prime))))
 
-; port -> (or number #f)
-(define (next-odd-palindrome/13 port)
-  ; rewind port if necessary, so that
-  ; overlapping palidromes are considered
-  (let ([pos (file-position port)])
-    (unless (= 0 pos)
-      (file-position port (- pos 7))))
-  (let ([match->number (compose1 string->number bytes->string/utf-8 car)])
-    (cond [(regexp-match #px"([13579])(\\d)(\\d)(\\d)(\\d)(\\d)(\\d)\\6\\5\\4\\3\\2\\1" port) => match->number]
-          [else #f])))
+; number number -> boolean
+(define divisible?
+  (compose1 (curry = 0) remainder))
+
+; number string -> string
+(define (string-repeat n str)
+  (apply string-append (make-list n str)))
 
 ; list of the first million primes
 ; (ListOf number)
@@ -75,14 +75,32 @@ I can do that, since I have 7-digit primes already.
       (for/list ([n (in-producer read eof in)])
         n))))
 
-; number number -> boolean
-(define divisible?
-  (compose1 (curry = 0) remainder))
-
-; number -> boolean
-(define (is-prime? n)
-  (not
-   (for/or ([prime (stop-before (in-list primes/1M) (curry <= (sqrt n)))])
-     (divisible? n prime))))
+; tests
+(test
+ (call-with-input-string
+  "123457543212345734450605443"
+  (λ (s)
+    (for/list ([i (in-producer (next-odd-palindrome/gen 11) #f s)])
+      i)))
+ =>
+ '(12345754321
+   75432123457
+   34450605443)
+ 
+ (odd-palindrome-pattern 1)
+ =>
+ "([13579])"
+ 
+ (odd-palindrome-pattern 2)
+ =>
+ "([13579])\\1"
+ 
+ (odd-palindrome-pattern 3)
+ =>
+ "([13579])(\\d)\\1"
+ 
+ (odd-palindrome-pattern 11)
+ =>
+ "([13579])(\\d)(\\d)(\\d)(\\d)(\\d)\\5\\4\\3\\2\\1")
 
 (main)
